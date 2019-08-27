ce502ed15b854323cd9a8f01b8a7751b7284030a
s8sg <swarvanusg@gmail.com>
2019-08-27 18:02:10 +0900

    Fix issue with executor
    Signed-off-by: s8sg <swarvanusg@gmail.com>

--- template/faas-flow/default_state_store.go
+++ template/faas-flow/default_state_store.go
@@ -6,19 +6,14 @@ import (
 )
 
 type DefaultStateStore struct {
-	flowName     string
-	requestId    string
-	keyDecorator func(string) string
-	cache        map[string]string
-	mux          sync.Mutex
+	flowName  string
+	requestId string
+	cache     map[string]string
+	mux       sync.Mutex
 }
 
 // Configure the StateStore with flow name and request ID
 func (ss *DefaultStateStore) Configure(flowName string, requestId string) {
-	ss.keyDecorator = func(key string) string {
-		dKey := flowName + "-" + requestId + "-" + key
-		return dKey
-	}
 }
 
 // Initialize the StateStore (called only once in a request span)
@@ -30,7 +25,7 @@ func (ss *DefaultStateStore) Init() error {
 // Set a value (override existing, or create one)
 func (ss *DefaultStateStore) Set(key string, value string) error {
 	ss.mux.Lock()
-	ss.cache[ss.keyDecorator(key)] = value
+	ss.cache[key] = value
 	ss.mux.Unlock()
 	return nil
 }
@@ -38,7 +33,7 @@ func (ss *DefaultStateStore) Set(key string, value string) error {
 // Get a value
 func (ss *DefaultStateStore) Get(key string) (string, error) {
 	ss.mux.Lock()
-	value, ok := ss.cache[ss.keyDecorator(key)]
+	value, ok := ss.cache[key]
 	ss.mux.Unlock()
 	if !ok {
 		return "", fmt.Errorf("key not found")
@@ -49,7 +44,7 @@ func (ss *DefaultStateStore) Get(key string) (string, error) {
 // Compare and Update a value
 func (ss *DefaultStateStore) Update(key string, oldValue string, newValue string) error {
 	ss.mux.Lock()
-	value, ok := ss.cache[ss.keyDecorator(key)]
+	value, ok := ss.cache[key]
 	if !ok {
 		ss.mux.Unlock()
 		return fmt.Errorf("key not found")
@@ -58,7 +53,7 @@ func (ss *DefaultStateStore) Update(key string, oldValue string, newValue string
 		ss.mux.Unlock()
 		return fmt.Errorf("value doesn't match")
 	}
-	ss.cache[ss.keyDecorator(key)] = newValue
+	ss.cache[key] = newValue
 	ss.mux.Unlock()
 	return nil
 }
