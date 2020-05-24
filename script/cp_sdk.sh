9afa555fddf8a3650900515c01489885f5865828
pasdam <pasdam@users.noreply.github.com>
2020-05-24 20:04:39 +0800

    Remove old script

--- script/cp_sdk.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-(cd .. && rm -rf template/faas-flow/vendor/github.com/s8sg/faas-flow/*)
-(cd .. && cp -r sdk workflow.go faas_operation.go template/faas-flow/vendor/github.com/s8sg/faas-flow/)
